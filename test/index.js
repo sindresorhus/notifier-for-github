b19cc42e669d02063d41841d4167853e8a17a240
Sindre Sorhus <sindresorhus@gmail.com>
2015-07-12 22:41:51 +0200

    cleanup #24

--- test/index.js
+++ test/index.js
@@ -17,37 +17,40 @@ global.window = {
 		search: ''
 	}
 };
+
 function XMLHttpRequest() {
 	this.requestHeaders = {};
 	this.responseHeaders = {};
 }
 
-XMLHttpRequest.prototype.open = function open(method, url) {
+XMLHttpRequest.prototype.open = function (method, url) {
 	this.method = method;
 	this.url = url;
 };
 
-XMLHttpRequest.prototype.send = function send() {
+XMLHttpRequest.prototype.send = function () {
 	this.readyState = 4;
 	this.responseText = '[]';
 	this.status = 200;
 	this.responseHeaders['X-Poll-Interval'] = '60';
+
 	if (this.onreadystatechange) {
 		setTimeout(this.onreadystatechange, 50);
 	}
 };
 
-XMLHttpRequest.prototype.setRequestHeader = function setRequestHeader(name, val) {
+XMLHttpRequest.prototype.setRequestHeader = function (name, val) {
 	this.requestHeaders[name] = val;
 };
 
-XMLHttpRequest.prototype.getHeader = function getHeader(name) {
+XMLHttpRequest.prototype.getHeader = function (name) {
 	return this.responseHeaders[name];
 };
 
 global.XMLHttpRequest = XMLHttpRequest;
 global.localStorage = new LocalStorageMock();
 global.chrome = chromeStub;
+
 describe('basic functionality', function () {
 	it('should register alarm callback', function () {
 		require('../extension/api');
@@ -57,7 +60,9 @@ describe('basic functionality', function () {
 
 	it('should call gitHubNotifCount on chrome alarm fired', function (done) {
 		var oldNotifCount = global.window.gitHubNotifCount;
+
 		global.window.gitHubNotifCount = sinon.stub();
+
 		process.nextTick(function () {
 			chromeStub.alarms.onAlarm.trigger();
 			assert(global.window.gitHubNotifCount.called);

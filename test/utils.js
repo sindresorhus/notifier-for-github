ad55879c43fbcf2cd40c18e3be0213017a459eae
YurySolovyov <biohazard707@gmail.com>
2016-08-05 11:18:00 +0300

    Drop custom stubs

--- test/utils.js
+++ test/utils.js
@@ -13,10 +13,6 @@ module.exports = {
 				getItem: () => {}
 			},
 			chrome: Object.assign({}, chromeStub, {
-				permissions: {
-					request: () => {},
-					contains: () => {}
-				},
 				runtime: {}
 			})
 		};

562069da541fab074a3caafa241c8753fef05711
Yury Solovyov <biohazard707@gmail.com>
2017-04-06 22:54:07 +0300

    Fix option tests in sync mode

--- test/option-test.js
+++ test/option-test.js
@@ -4,6 +4,7 @@ import sinon from 'sinon';
 import util from './util';
 
 global.window = util.setupWindow();
+const sandbox = sinon.sandbox.create();
 
 const PersistenceService = require('../extension/src/persistence-service.js');
 const Option = require('../extension/src/option.js');
@@ -25,29 +26,37 @@ test.beforeEach(t => {
 		onChange() {}
 	};
 	t.context.option = new Option(t.context.optionParams);
+	window.chrome.storage.sync = { get: () => {} };
 });
 
-test('#readValue reads value from PersistenceService', t => {
+test.afterEach(t => {
+	sandbox.restore();
+});
+
+test('#readValue reads value from PersistenceService', async t => {
 	window.localStorage.getItem = sinon.stub().returns('value');
 
-	t.context.option.readValue();
+	sandbox.stub(window.chrome.storage.sync, 'get')
+		.withArgs('key').yieldsAsync('value');
 
-	t.true(window.localStorage.getItem.calledWith('key'));
+	await t.context.option.readValue();
+
+	t.true(window.chrome.storage.sync.get.calledWith('key'));
 	t.is(t.context.option.element.value, 'value');
 });
 
-test('#writeValue writes value to PersistenceService', t => {
+test('#writeValue writes value to PersistenceService', async t => {
 	PersistenceService.set = sinon.stub();
 	t.context.option.element.value = 'value';
-	t.context.option.writeValue();
+	await t.context.option.writeValue();
 
 	t.true(PersistenceService.set.calledWith('key', 'value'));
 });
 
-test('#writeValue respects override', t => {
+test('#writeValue respects override', async t => {
 	PersistenceService.set = sinon.stub();
 	t.context.option.element.value = 'defaultValue';
-	t.context.option.writeValue('newValue');
+	await t.context.option.writeValue('newValue');
 
 	t.true(PersistenceService.set.calledWith('key', 'newValue'));
 	t.is(t.context.option.element.value, 'newValue');

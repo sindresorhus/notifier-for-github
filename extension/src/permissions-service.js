009b6cdfe5c56ed8b48d969dd7278386411e4de8
ExE Boss <ExE-Boss@users.noreply.github.com>
2017-06-07 18:32:20 +0200

    Add support for Firefox 52
      Also fixes tab opening when pressing the toolbar button in Firefox 54
    and earlier. This fixes #127.

--- extension/src/permissions-service.js
+++ extension/src/permissions-service.js
@@ -3,28 +3,36 @@ const PersistenceService = require('./persistence-service.js');
 const PermissionsService = {
 	requestPermission(permission) {
 		return new Promise((resolve, reject) => {
-			window.chrome.permissions.request({
-				permissions: [permission]
-			}, granted => {
-				if (window.chrome.runtime.lastError) {
-					return reject(window.chrome.runtime.lastError);
-				}
-				PersistenceService.set(`${permission}_permission`, granted);
-				resolve(granted);
-			});
+			if (window.chrome.permissions) {
+				window.chrome.permissions.request({
+					permissions: [permission]
+				}, granted => {
+					if (window.chrome.runtime.lastError) {
+						return reject(window.chrome.runtime.lastError);
+					}
+					PersistenceService.set(`${permission}_permission`, granted);
+					resolve(granted);
+				});
+			} else {
+				resolve(undefined);
+			}
 		});
 	},
 
 	queryPermission(permission) {
 		return new Promise((resolve, reject) => {
-			window.chrome.permissions.contains({
-				permissions: [permission]
-			}, granted => {
-				if (window.chrome.runtime.lastError) {
-					return reject(window.chrome.runtime.lastError);
-				}
-				resolve(granted);
-			});
+			if (window.chrome.permissions) {
+				window.chrome.permissions.contains({
+					permissions: [permission]
+				}, granted => {
+					if (window.chrome.runtime.lastError) {
+						return reject(window.chrome.runtime.lastError);
+					}
+					resolve(granted);
+				});
+			} else {
+				resolve(undefined);
+			}
 		});
 	}
 };

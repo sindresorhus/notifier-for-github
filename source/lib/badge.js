182452fac3e1e210fd0ed4bbfff4f69d9343b719
Federico <me@fregante.com>
2021-04-07 05:28:19 +0700

    Lint

--- source/lib/badge.js
+++ source/lib/badge.js
@@ -1,3 +1,4 @@
+import browser from 'webextension-polyfill';
 import * as defaults from './defaults';
 
 function render(text, color, title) {
--- source/lib/tabs-service.js
+++ source/lib/tabs-service.js
@@ -1,3 +1,4 @@
+import browser from 'webextension-polyfill';
 import optionsStorage from '../options-storage';
 import {isChrome} from '../util';
 

1227c78f0c6f38dee071a8e2d18e3bc37f44b3cc
Laxman Damera <bytemode@outlook.com>
2023-02-06 21:58:25 +0530

    Save lastModified locally as GMT string

--- source/lib/api.js
+++ source/lib/api.js
@@ -126,7 +126,7 @@ export async function getNotificationCount() {
 	const {headers, json: notifications} = await getNotificationResponse({maxItems: 1});
 
 	const interval = Number(headers.get('X-Poll-Interval'));
-	const lastModified = (new Date(headers.get('Last-Modified'))).toISOString();
+	const lastModified = (new Date(headers.get('Last-Modified'))).toGMTString();
 	const linkHeader = headers.get('Link');
 
 	if (linkHeader === null) {
